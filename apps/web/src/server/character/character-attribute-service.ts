7e870876171a77b8b008b2cdd03bdbb5bd232726
Nicholas JM <283436098+ZeiJM@users.noreply.github.com>
2026-09-07 06:04:34 -0400

    Format attribute service

--- apps/web/src/server/character/character-attribute-service.ts
+++ apps/web/src/server/character/character-attribute-service.ts
@@ -142,7 +142,5 @@ function sumAttributes(attributes: CharacterAttributes): number {
 }
 
 function isUuid(value: string): boolean {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
-    value,
-  )
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value)
 }
