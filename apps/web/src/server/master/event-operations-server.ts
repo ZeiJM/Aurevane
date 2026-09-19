402d465def97b97b343cc41915e81557de27cf92
Nicholas JM <283436098+ZeiJM@users.noreply.github.com>
2026-09-19 09:44:22 -0400

    style(events): format live operations

--- apps/web/src/server/master/event-operations-server.ts
+++ apps/web/src/server/master/event-operations-server.ts
@@ -1,7 +1,10 @@
 import 'server-only'
 
 import { createServerMasterPanelStaffAccessService } from './staff-access-server'
-import { createEventOperationsService, type EventOperationsService } from './event-operations-service'
+import {
+  createEventOperationsService,
+  type EventOperationsService,
+} from './event-operations-service'
 import { createSupabaseEventOperationsStore } from './supabase-event-operations-store'
 
 export function createServerEventOperationsService(): EventOperationsService {
