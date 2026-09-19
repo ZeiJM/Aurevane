72ab71dafecb2cc67097c984193a7d5f2372a0cd
Nicholas JM <283436098+ZeiJM@users.noreply.github.com>
2026-09-19 01:23:31 -0400

    feat(master): wire live Event operations server

---
+++ apps/web/src/server/master/event-operations-server.ts
@@ -0,0 +1,12 @@
+import 'server-only'
+
+import { createServerMasterPanelStaffAccessService } from './staff-access-server'
+import { createEventOperationsService, type EventOperationsService } from './event-operations-service'
+import { createSupabaseEventOperationsStore } from './supabase-event-operations-store'
+
+export function createServerEventOperationsService(): EventOperationsService {
+  return createEventOperationsService({
+    store: createSupabaseEventOperationsStore(),
+    staffAccess: createServerMasterPanelStaffAccessService(),
+  })
+}
