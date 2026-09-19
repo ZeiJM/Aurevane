0adf28c170b3822c2cc92105f9784aa547619eed
Nicholas JM <283436098+ZeiJM@users.noreply.github.com>
2026-09-19 09:19:45 -0400

    style(events): format Event Builder

--- apps/web/src/server/master/event-authoring-server.ts
+++ apps/web/src/server/master/event-authoring-server.ts
@@ -1,10 +1,7 @@
 import 'server-only'
 
 import { createServerMasterPanelStaffAccessService } from './staff-access-server'
-import {
-  createEventAuthoringService,
-  type EventAuthoringService,
-} from './event-authoring-service'
+import { createEventAuthoringService, type EventAuthoringService } from './event-authoring-service'
 import { createSupabaseEventAuthoringStore } from './supabase-event-authoring-store'
 
 export function createServerEventAuthoringService(actorUserId: string): EventAuthoringService {
